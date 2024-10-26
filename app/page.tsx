0ca9ea9e3230b179fc8deec30e4e44b5cb163109
Ilango <rajagopalilango@gmail.com>
2024-10-27 04:32:33 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
 	return (
@@ -15,35 +16,27 @@ export default function Home() {
 				/>
 				<ol>
 					<li>
-						Get started by editing <code>app/page.tsx</code>.
+						Get started by editing <code>keystatic.config.ts</code>.
 					</li>
 					<li>Save and see your changes instantly.</li>
 				</ol>
 
 				<div className={styles.ctas}>
-					<a
+					<Link
 						className={styles.primary}
-						href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-						target='_blank'
+						href='/keystatic'
 						rel='noopener noreferrer'
 					>
-						<Image
-							className={styles.logo}
-							src='/vercel.svg'
-							alt='Vercel logomark'
-							width={20}
-							height={20}
-						/>
-						Deploy now
-					</a>
-					<a
-						href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+						Keystatic
+					</Link>
+					<Link
+						href='https://keystatic.com/docs/introduction'
 						target='_blank'
 						rel='noopener noreferrer'
 						className={styles.secondary}
 					>
-						Read our docs
-					</a>
+						Keystatic Documentation
+					</Link>
 				</div>
 			</main>
 			<footer className={styles.footer}>
